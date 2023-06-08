dbbf1e62e32d5fd1d72baa3c49cf6c177746cbe8
maku1111 <92520944+maku1111@users.noreply.github.com>
2023-06-08 20:49:02 +0800

    email-versenden funktioniert, allerdings ohne bild bisher. dies wird als nächstes implementiert.

--- CreatePostcard/views.py
+++ CreatePostcard/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 import sqlite3
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.core.mail import EmailMessage, get_connection, send_mail
+from django.conf import settings
+
 
 
 @login_required
@@ -10,35 +13,44 @@ def createpostcardaction(request):
     print('before post.')
     if request.method=="POST":
         print('start of post.')
-        salutation = request.POST.get('salutation')
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        email = request.POST.get('email')
-        postcard_message = request.POST.get('message')
+        rec_salutation = request.POST.get('salutation')
+        rec_first_name = request.POST.get('first_name')
+        rec_last_name = request.POST.get('last_name')
+        rec_email = request.POST.get('email')
+        rec_postcard_message = request.POST.get('message')
 
         print('read recipient details.')
 
         # Daten in SQLite-Datenbank einfügen
         connection = sqlite3.connect('MyDatabase.db')
-        cursor=connection.cursor()
-        # Beispielabfrage ausführen
-        cursor.execute("SELECT * FROM recipients")
-
-        # Alle Zeilen aus der Abfrage abrufen
-        rows = cursor.fetchall()
-
-        # Ergebnis anzeigen
-        for row in rows:
-            print(row)
-
-        # Verbindung schließen
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO recipients (salutation, first_name, last_name, email, postcard_message) VALUES (?, ?, ?, ?, ?)",
+                     (rec_salutation, rec_first_name, rec_last_name, rec_email, rec_postcard_message))
+        connection.commit()
         connection.close()
-        #cursor = connection.cursor()
-        #cursor.execute("INSERT INTO recipients (salutation, first_name, last_name, email, postcard_message) VALUES (?, ?, ?, ?, ?)",
-        #             (salutation, first_name, last_name, email, postcard_message))
-        #connection.commit()
-        #connection.close()
 
-        print('done.')
+        print('done writing to database.')
+        print('send email')
+
+
+        subject="You received an E-postcard"
+    
+     
+        send_mail(subject,rec_postcard_message,settings.EMAIL_HOST_USER,[rec_email])
+
+        #with get_connection(  
+         #   host=settings.EMAIL_HOST, 
+          #  port=settings.EMAIL_PORT,  
+           # username=settings.EMAIL_HOST_USER, 
+            #password=settings.EMAIL_HOST_PASSWORD, 
+            #use_tls=settings.EMAIL_USE_TLS
+        #) as connection:  
+         #   subject = 'You received an E-Postcard' 
+          #  email_from = settings.EMAIL_HOST_USER  
+           # recipient_list = email  
+            #message = postcard_message 
+            #EmailMessage(subject, message, email_from, recipient_list, connection=connection).send()
+
 
     return render(request,'Create_your_postcard.html')
+ 
--- MyPostCardService/settings.py
+++ MyPostCardService/settings.py
@@ -132,3 +132,12 @@ STATICFILES_DIRS = [
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_SSL=False
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'Mypostcardservice@gmail.com'
+EMAIL_HOST_PASSWORD = 'muxqsbrfytzkxwco'
