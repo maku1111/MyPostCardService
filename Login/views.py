ba1fe19a89d3d6d1fb767797b081ce03a50229a4
maku1111 <92520944+maku1111@users.noreply.github.com>
2023-06-10 02:29:19 +0800

    deletet error.html and added error.message()

--- Login/views.py
+++ Login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
 import mysql.connector as sql
 
 # Create your views here.
@@ -13,6 +14,9 @@ def loginaction(request):
             login(request, user)
             return redirect('createpostcard')
         else:
-            return render(request, 'error.html')
+            messages.error(request,'Email or Password is wrong. Please log in again.')
+            return redirect('login')
+            #implement something here
+            #return render(request, 'error.html')
 
     return render(request,'Login.html')
