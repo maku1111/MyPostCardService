13c816f9a3bfda4a85c9ba59150ad57f8a9172d7
maku1111 <92520944+maku1111@users.noreply.github.com>
2023-06-12 01:07:24 +0800

    added allowed host

--- MyPostCardService/settings.py
+++ MyPostCardService/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#%gp5zkt$$hph+bz!=a7w_%*h2ni6&ut+-39kwes_9#&-hhz+7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.53.192.116']
 
 
 # Application definition
