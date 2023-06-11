67b4abb6c2aa28303b8c29a22be3bb5e09df35d4
maku1111 <92520944+maku1111@users.noreply.github.com>
2023-06-11 23:14:00 +0800

    deleted import

--- SignUp/views.py
+++ SignUp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
-import mysql.connector as sql
 
 
 # Create your views here.
