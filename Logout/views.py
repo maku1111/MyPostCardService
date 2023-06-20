fcc7ed648175e8d75e9719045add3677ad9a04bd
maku1111 <92520944+maku1111@users.noreply.github.com>
2023-06-20 23:53:33 +0800

    changed logout.css and login required for logout

--- Logout/views.py
+++ Logout/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
+@login_required
 # Create your views here.
 def logoutaction(request):
     
